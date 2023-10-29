44c45dad42bd1cc8968a71d3724be39cfb788649
Pedro Henrique <phmartins.developer@gmail.com>
2023-10-29 20:45:51 -0300

    <Style> [Weather page]

--- src/pages/Weather/index.tsx
+++ src/pages/Weather/index.tsx
@@ -54,7 +54,6 @@ export default function Weather() {
           value={city}
           onChange={handleInputChange}
           placeholder="City"
-          onFocus={() => setIsInputFocused(true)}
         />
         <Button
           onClick={() => {
